fd002acc8179c029a57d4d9fcf0db142cf7c1bf6
Swaraj Nanda <85413978+swarajnanda2021@users.noreply.github.com>
2022-09-25 10:48:52 +0100

    Update inspect_mpii.py
    added function for bounding box that prevents unlabelled personnel from getting penalized while training

--- data_tools/inspect_mpii.py
+++ data_tools/inspect_mpii.py
@@ -14,13 +14,11 @@ import csv
 # load mpii dataset that you use saved as a csv file
 dataset_mpii = pd.read_csv('../mpii/dataset.csv')
 
-# define a 1d Gaussian kernel from the paper
 def gaussiankernel1D(x,x_p,sigma): # all entries will be pixel level resolution
     # Gaussian kernel: np.exp(np.abs(x - x_p)**2)/sigma**2
     kernel = np.exp(-np.linalg.norm(x - x_p)**2/sigma**2)
     return kernel
-  
-# generate a 2D gaussian kernel for the keypoint 
+
 def gaussiankernel2D(halfwidth=3,spread=5):    
     x_space =  np.linspace(-halfwidth,halfwidth,2*halfwidth+1)
     kernel = np.zeros_like(x_space)
@@ -29,8 +27,7 @@ def gaussiankernel2D(halfwidth=3,spread=5):
     kernel=np.expand_dims(kernel, axis = 0)
     TwoDkernel = kernel*(kernel.T)
     return TwoDkernel#, plt.imshow(TwoDkernel)
-
-# generate a heatmap for a single keypoint (survives keypoints too close to image boundaries by padding, adding and then center cropping)
+    
 def generateHeatmap1Joint(imgsize=[1024,1024],posx=1000,posy=600,kernelhalfwidth=100,spread=30):  
     
     img = np.zeros((imgsize[1], imgsize[0]), dtype=float)
@@ -51,78 +48,108 @@ def generateHeatmap1Joint(imgsize=[1024,1024],posx=1000,posy=600,kernelhalfwidth
     
     return A
 
-# generate the PAF of the limbs
-def generatePAF(imgsize,links):
-    # work in prog
-    img = np.zeros((imgsize[1], imgsize[0]), dtype=float)
+
+def generateIgnoreMask(imgsize, bbox):
+    # pre allocate
+    ignoremask = np.zeros((imgsize[1],imgsize[0],3))
+    # set roi to 1 based on information from bbox array
+    ignoremask[int(bbox[1]):int(bbox[3]),int(bbox[0]):int(bbox[2]),0:3] = 1
     
+    return ignoremask
     
+
+def generatePAF(imgsize,keypoints,partconnections):
     
+    paf = 0    
     
-    
-    return img
+    return paf
 
 
+#%% Cell for performing data augmentation to an image and corresponding keypoints
 
+#%% Cell for inspecting a particular image
 
-# define a function that inspects the images and generates heatmap and part affinity fields
+# define a function that inspects the images and generates heatmap, part affinity fields and person mask
+
 def imag(number):
-    #img = datfrm['NAME'][number]
-    #print(img)
-    data_sub1 = dataset_mpii.loc[dataset_mpii['ImgNumber'] == number]  # Get rows with particular value
-    if np.size(data_sub1)==0: # do not plot image numbers that have no data (super strange)
+    
+    data_sub1 = dataset_mpii.loc[dataset_mpii['ImgNumber'] == number]                       # Get rows with particular value
+    if np.size(data_sub1)==0:
         return print('Bad data')
     
-    
-    print(data_sub1) 
-    img = data_sub1.iloc[0,1]
-    
-    
-    traintrue = data_sub1.iloc[0,4]
-    print(traintrue)
-    
-    nPersons = data_sub1.iloc[0,2]
-    
+    img = data_sub1.iloc[0,0]
     im = Image.open('../mpii/images/'+img) 
-    print(im.size)
     
+    nPersons = int(data_sub1.iloc[0,1])
     
     
     heatmap = np.zeros((im.size[1], im.size[0]), dtype=float)
-    for person in range(0,data_sub1.shape[0]): # loop over people
+    
+    for person in range(nPersons):
+        
+        
+        # Generate mask
+        keypoints = np.array(data_sub1.iloc[person,5:33])
+        
+        traintrue = data_sub1.iloc[person,3]
+        
+        
+        
+        # collect masking data Estimate ignore_mask
+        bbox = np.array(data_sub1.iloc[person,36:40])
+        
+        
+        
+        print(im.size)
+        
+        mask = generateIgnoreMask(im.size,bbox)
+        
+        
+        print(np.shape(mask))
+        
+        
+        maskedimg = im*mask/255
+        
+        plt.imshow(maskedimg)
+        plt.show()
+        
+        
+        
+        
         print(person)
         implot = plt.imshow(im)
         # plot keypoints
-        px = 5
-        while px <37:        
-            
+        px = 4
+        while px <36:        
             # plot keypoints using your data
             x = data_sub1.iloc[person,px]
             y = data_sub1.iloc[person,px+1]
             px=px+2
-            
-            if x<0 and y<0: # do not plot the -1s
+            if x<0 and y<0:
                 continue
-            
-            
             print(x,y)
             print(y,np.size(im,1))
-            
             if x<np.size(im,0) or y<np.size(im,1):
                 #print(col,x,y)
                 plt.scatter([[x]],[[y]])
                 # make heatmaps
                 heatmap += generateHeatmap1Joint(imgsize=im.size,posx=int(y),posy=int(x),kernelhalfwidth=200,spread=30)
-                
         plt.show()
-            
+    
     implot2 = plt.imshow(heatmap)
     plt.colorbar()
     plt.show()
     
+    return maskedimg,data_sub1
+    
+
+
+
+
+   
     
     
 
 if __name__ == '__main__':
-    # run this to plot and see the heatmaps and keypoint data
-    imag(10)
+    
+    imag(129)
